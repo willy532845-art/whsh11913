d22199a4a88858883ad120af0fd5b9d3d7c22a56
willy532845-art <willy532845@gmail.com>
2025-10-26 10:45:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 
+pip install requests
+
+import requests
+
+# Define the API endpoint URL
+api_url = "https://uselessfacts.jsph.pl/random.json?language=en`"
+
+# Make a GET request (for retrieving data)
+response = requests.get(api_url)
+
+# Make a POST request (for sending data)
+# data_payload = {"key": "value"}
+# response = requests.post(api_url, json=data_payload)
