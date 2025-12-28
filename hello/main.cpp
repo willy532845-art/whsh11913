9586dffd66491d24e8c625592f27b491aa424944
willy <willy532845@gmail.com>
2025-12-28 23:11:15 +0800

    update files

--- hello/main.cpp
+++ hello/main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world" << endl;
-    cin.get();  // 等你按 Enter 再結束
+    int day;
+    cout << "Enter day (1-7): ";
+    cin >> day;
+
+    switch (day) {
+        case 1: cout << "Mon\n"; break;
+        case 2: cout << "Tue\n"; break;
+        case 3: cout << "Wed\n"; break;
+        case 4: cout << "Thu\n"; break;
+        case 5: cout << "Fri\n"; break;
+        case 6: cout << "Sat\n"; break;
+        case 7: cout << "Sun\n"; break;
+        default: cout << "Invalid (1-7)\n";
+    }
+
+    cin.ignore();
+    cin.get();
     return 0;
 }
