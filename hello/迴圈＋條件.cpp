14365ccb4a295128e57c7e386b801b515d61d001
willy532845-art <willy532845@gmail.com>
2025-12-31 11:31:11 +0800

    迴圈＋條件

--- hello/迴圈＋條件.cpp
+++ hello/迴圈＋條件.cpp
@@ -1,15 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int N;
-    int a=0;
-    cin>>N;
-    for(int i=1;i<=N;i++){
-        if(i%3==0&&i%5!=0){
-            a++;
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    int a[N + 1] = {0};
+
+    for (int j = 0; j < M; j++) {
+        int K;
+        cin >> K;
+        for (int i = K; i <= N; i += K) {
+            a[i] = 1 - a[i];
         }
     }
-    cout<<a;
+
+    int cou = 0;
+    for (int i = 1; i <= N; i++) {
+        if (a[i] == 1) cou++;
+    }
+
+    cout << cou;
     return 0;
 }
